af00d51465355531e19789a959f02437ebbf212b
Aaron Gaither <AaronGaither@coresystems.local>
2017-06-15 01:20:24 -0700

    add spotify functionality

--- keys.js
+++ keys.js
@@ -1,8 +1,11 @@
-console.log('Keys loaded.');
-
 exports.twitter = {
-  consumer_key: 'QXKUHCJQ7254mCrQuyrFZF403',
-  consumer_secret: 'XE6Q0ZccRLjPoebwicgiRJrKAtBR9NV9UBNYs0xSuRVxZ5msnr',
-  access_token_key: '22541823-yg42osWdxy8fadklq6Py05ZcV8hfIgx55EDeeKZSQ',
-  access_token_secret: 'APLPIXacqZgoEnIGhOGZ0VyiANwvjscjMN4hR6NPbYBZx'
+    consumer_key: 'QXKUHCJQ7254mCrQuyrFZF403',
+    consumer_secret: 'XE6Q0ZccRLjPoebwicgiRJrKAtBR9NV9UBNYs0xSuRVxZ5msnr',
+    access_token_key: '22541823-yg42osWdxy8fadklq6Py05ZcV8hfIgx55EDeeKZSQ',
+    access_token_secret: 'APLPIXacqZgoEnIGhOGZ0VyiANwvjscjMN4hR6NPbYBZx'
+}
+
+exports.spotify = {
+    clientID: '0cdfaea591014369a3ed5044f1ea09d9',
+    clientSecret: '8db930c41a694e55a32e22003901a0bd'
 }
--- liri.js
+++ liri.js
@@ -5,38 +5,56 @@ let userCommand = process.argv[2];
 let option = process.argv[3];
 
 const commands = {
-	'my-tweets': () => {
-		console.log('tweets');
-		let Twitter = require('twitter');
-		let client = new Twitter({
-		  consumer_key: keys.twitter.consumer_key,
-		  consumer_secret: keys.twitter.consumer_secret,
-		  access_token_key: keys.twitter.access_token_key,
-		  access_token_secret:  keys.twitter.access_token_secret
-		});
+    'my-tweets': () => {
+        let Twitter = require('twitter');
+        let client = new Twitter({
+          consumer_key: keys.twitter.consumer_key,
+          consumer_secret: keys.twitter.consumer_secret,
+          access_token_key: keys.twitter.access_token_key,
+          access_token_secret:  keys.twitter.access_token_secret
+        });
 
-		client.get('/statuses/user_timeline.json', { count: 20 }, function(error, tweets, response) {
-		  if(error) throw error;
-		  tweets.forEach( function(element, index) {
-		  	let tweet = element.text;
-		  	let d = new Date(element.created_at).toDateString();
-		  	console.log(d, tweet)
-		  });
-		});
-	},
-	'spotify-this-song': (option) => {
-		let song = option || "The Sign";
-		console.log(`Spotify: ${song}`);
-	}, 
-	'movie-this': (option) => {
-		let movie = option || "Mr. Nobody";
-		console.log(`Movie: ${movie}`);
-	}, 
-	'do-what-it-says': () => {
-		let lines = fs.readFileSync('random.txt').toString().replace('\r','').split('\n');
-		let [userCommand, option] = lines[Math.floor(Math.random() * lines.length)].split(',');
-		commands[userCommand](option)
-	}
+        client.get('/statuses/user_timeline.json', { count: 20 }, function(error, tweets, response) {
+            if ( err ) {
+                console.log('Error occurred: ' + err);
+                return;
+            }
+            tweets.forEach( function(element) {
+                let tweet = element.text;
+                let d = new Date(element.created_at).toDateString();
+                console.log(d, tweet)
+            });
+        });
+    },
+    'spotify-this-song': (option) => {
+        let song = option || "Jungle Love";
+        let Spotify = require('node-spotify-api');
+        let client = new Spotify({
+          id: keys.spotify.clientID,
+          secret: keys.spotify.clientSecret
+        });
+         
+        client.search({ type: 'track', query: song }, function(err, data) {
+            if ( err ) {
+                console.log('Error occurred: ' + err);
+                return;
+            }
+            let songData = data.tracks.items[0];
+            console.log("Artist:",songData.artists[0].name)
+            console.log("Song Title:",songData.name)
+            console.log("Album:",songData.album.name)
+            console.log("Link:",songData.preview_url)
+        });
+    }, 
+    'movie-this': (option) => {
+        let movie = option || "Mr. Nobody";
+        console.log(`Movie: ${movie}`);
+    }, 
+    'do-what-it-says': () => {
+        let lines = fs.readFileSync('random.txt').toString().replace('\r','').split('\n');
+        let [userCommand, option] = lines[Math.floor(Math.random() * lines.length)].split(',');
+        commands[userCommand](option)
+    }
 };
 
 !(userCommand in commands) ? console.log('Command not recognized. I am not THAT smart...') : commands[userCommand](option)
