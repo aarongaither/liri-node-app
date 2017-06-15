7600403ebc6ba9e078475daf5d306696fd845d79
Aaron Gaither <AaronGaither@coresystems.local>
2017-06-15 02:26:19 -0700

    add logging functionality

--- liri.js
+++ liri.js
@@ -4,6 +4,13 @@ let fs = require('fs');
 let userCommand = process.argv[2];
 let option = process.argv[3];
 
+let logger = fs.createWriteStream('log.txt', {flags: 'a'});
+
+function logData (data) {
+    logger.write(data+'\n');
+    console.log(data)
+}
+
 const commands = {
     'my-tweets': () => {
         let Twitter = require('twitter');
@@ -19,11 +26,13 @@ const commands = {
                 console.log('Error occurred: ' + err);
                 return;
             }
+            logData('__My tweets__')
             tweets.forEach( function(element) {
                 let tweet = element.text;
                 let d = new Date(element.created_at).toDateString();
-                console.log(d, tweet)
+                logData(`${d}: ${tweet}`)
             });
+            logData('\r')
         });
     },
     'spotify-this-song': (option) => {
@@ -40,10 +49,12 @@ const commands = {
                 return;
             }
             let songData = data.tracks.items[0];
-            console.log("Artist:",songData.artists[0].name)
-            console.log("Song Title:",songData.name)
-            console.log("Album:",songData.album.name)
-            console.log("Link:",songData.preview_url)
+            logData('__spotify-this-song__')
+            logData(`Artist: ${songData.artists[0].name}`)
+            logData(`Song Title: ${songData.name}`)
+            logData(`Album: ${songData.album.name}`)
+            logData(`Link: ${songData.preview_url}`)
+            logData('\r')
         });
     }, 
     'movie-this': (option) => {
@@ -61,14 +72,17 @@ const commands = {
                 console.log('Error occurred: ' + err);
                 return;
             }
+            logData('__movie-this__')
             movieAtt.forEach( function(element, index) {
-                console.log(`${element}: ${body[element]}`);
+                logData(`${element}: ${body[element]}`);
             });
+            logData('\r')
         })
     }, 
     'do-what-it-says': () => {
         let lines = fs.readFileSync('random.txt').toString().replace('\r','').split('\n');
         let [userCommand, option] = lines[Math.floor(Math.random() * lines.length)].split(',');
+        logData('__do-what-it-says__')
         commands[userCommand](option)
     }
 };
