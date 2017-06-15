44796268a2908fe15c94f95b8aae580cb493aa73
Aaron Gaither <AaronGaither@coresystems.local>
2017-06-15 00:49:31 -0700

    add twitter functionality

--- keys.js
+++ keys.js
@@ -1,8 +1,8 @@
 console.log('Keys loaded.');
 
 exports.twitter = {
-  consumer_key: '<input here>',
-  consumer_secret: '<input here>',
-  access_token_key: '<input here>',
-  access_token_secret: '<input here>',
+  consumer_key: 'QXKUHCJQ7254mCrQuyrFZF403',
+  consumer_secret: 'XE6Q0ZccRLjPoebwicgiRJrKAtBR9NV9UBNYs0xSuRVxZ5msnr',
+  access_token_key: '22541823-yg42osWdxy8fadklq6Py05ZcV8hfIgx55EDeeKZSQ',
+  access_token_secret: 'APLPIXacqZgoEnIGhOGZ0VyiANwvjscjMN4hR6NPbYBZx'
 }
--- liri.js
+++ liri.js
@@ -5,14 +5,37 @@ let userCommand = process.argv[2];
 let option = process.argv[3];
 
 const commands = {
-	'my-tweets': () => {console.log('tweets')},
-	'spotify-this-song': (option) => {console.log(`Spotify: ${option}`)}, 
-	'movie-this': (option) => {console.log(`Movie: ${option}`)}, 
+	'my-tweets': () => {
+		console.log('tweets');
+		let Twitter = require('twitter');
+		let client = new Twitter({
+		  consumer_key: keys.twitter.consumer_key,
+		  consumer_secret: keys.twitter.consumer_secret,
+		  access_token_key: keys.twitter.access_token_key,
+		  access_token_secret:  keys.twitter.access_token_secret
+		});
+
+		client.get('/statuses/user_timeline.json', { count: 20 }, function(error, tweets, response) {
+		  if(error) throw error;
+		  tweets.forEach( function(element, index) {
+		  	let tweet = element.text;
+		  	let d = new Date(element.created_at).toDateString();
+		  	console.log(d, tweet)
+		  });
+		});
+	},
+	'spotify-this-song': (option) => {
+		let song = option || "The Sign";
+		console.log(`Spotify: ${song}`);
+	}, 
+	'movie-this': (option) => {
+		let movie = option || "Mr. Nobody";
+		console.log(`Movie: ${movie}`);
+	}, 
 	'do-what-it-says': () => {
-		let file = fs.readFileSync('random.txt');
-		let lines = file.toString().replace('\r','').split('\n');
-		let useLine = lines[Math.floor(Math.random() * lines.length)].split(',');
-		commands[useLine[0]](useLine[1])
+		let lines = fs.readFileSync('random.txt').toString().replace('\r','').split('\n');
+		let [userCommand, option] = lines[Math.floor(Math.random() * lines.length)].split(',');
+		commands[userCommand](option)
 	}
 };
 
